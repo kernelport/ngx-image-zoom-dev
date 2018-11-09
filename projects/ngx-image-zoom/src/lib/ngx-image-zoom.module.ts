f520da991d0ab652fec729a8b7d27c44297e7bb4
Frank Neuber <fn@kernelport.com>
2018-11-09 10:00:22 +0100

    integrate https://github.com/wittlock/ngx-image-zoom

--- projects/ngx-image-zoom/src/lib/ngx-image-zoom.module.ts
+++ projects/ngx-image-zoom/src/lib/ngx-image-zoom.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxImageZoomComponent } from './ngx-image-zoom.component';
+
+export * from './ngx-image-zoom.component';
 
 @NgModule({
   declarations: [NgxImageZoomComponent],
   imports: [
+	CommonModule
   ],
   exports: [NgxImageZoomComponent]
 })
-export class NgxImageZoomModule { }
+export class NgxImageZoomModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: NgxImageZoomModule,
+        };
+    }
+}
